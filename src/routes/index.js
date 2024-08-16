fe51cb26f4c3058e0072135dcf4786b2d61ce817
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-08-16 21:42:50 +0700

    handle_create_API_add_roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { userRoute } from './userRoute'
 import { bookRoute } from './bookRoute'
 import { groupRoute } from './groupRoute'
+import { roleRoute } from './roleRoute'
 
 const router = express.Router()
 
 router.use('/user', userRoute)
 router.use('/book', bookRoute)
 router.use('/group', groupRoute)
+router.use('/role', roleRoute)
 
 export const APIs = router
