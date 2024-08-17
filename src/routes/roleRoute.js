0f5e283e6e63c9f654437890f36c1e2357725593
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-08-17 15:37:39 +0700

    create_API_assign_role_to_group

--- src/routes/roleRoute.js
+++ src/routes/roleRoute.js
@@ -8,5 +8,6 @@ router.get('/read', roleController.getAllRoles)
 router.delete('/delete', roleController.deleteRole)
 router.put('/update', roleController.updateRole)
 router.get('/by-group/:groupId', roleController.getRoleByGroup)
+router.post('/assign-role-group', roleController.assignRoleToGroup)
 
 export const roleRoute = router
